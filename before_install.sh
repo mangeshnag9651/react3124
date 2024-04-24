c9bd17584d0ea436fc4ce5681f7be3f4e6b8c22e
mangeshnag9651 <98649695+mangeshnag9651@users.noreply.github.com>
2024-04-24 11:59:46 +0530

    Create before_install.sh

---
+++ before_install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+cd /var/www/html
+sudo rm -rf *
+cd 
+
+# Download the ZIP file from S3
+aws s3 cp s3://deployment3434/react-build.zip /home/ubuntu
+ 
+# Extract the ZIP file
+sudo tar -xvf react-build.zip --directory /var/www/html
+ 
+# Cleanup: remove the downloaded ZIP file if needed
+# rm /path/to/empty/folder/your-zip-file.zip
