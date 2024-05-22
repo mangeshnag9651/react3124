7d0ebd1897f5a8558dce39064bb28893d1cf1525
mangeshnag9651 <98649695+mangeshnag9651@users.noreply.github.com>
2024-05-22 17:34:08 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,8 +11,8 @@ resource "aws_route53_record" "abc" {
 
     alias {
         evaluate_target_health = true
-        name                   = var.use_Swap_lb ? var.test_lb_dns_name : var.prod_lb_dns_name
-        zone_id                = var.use_Swap_lb ? var.test_lb_zone_id : var.prod_lb_zone_id
+        name                   = var.use_Swap_lb ? var.prod_lb_dns_name : var.test_lb_dns_name
+        zone_id                = var.use_Swap_lb ? var.prod_lb_zone_id : var.test_lb_zone_id
     }
 }
 
@@ -24,8 +24,8 @@ resource "aws_route53_record" "test_abc" {
 
     alias {
         evaluate_target_health = true
-        name                   = var.use_Swap_lb ? var.prod_lb_dns_name : var.test_lb_dns_name
-        zone_id                = var.use_Swap_lb ? var.prod_lb_zone_id : var.test_lb_zone_id
+        name                   = var.use_Swap_lb ? var.test_lb_dns_name : var.prod_lb_dns_name
+        zone_id                = var.use_Swap_lb ? var.test_lb_zone_id : var.prod_lb_zone_id
     }
 }
 
