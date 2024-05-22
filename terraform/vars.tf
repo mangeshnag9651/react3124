dd8e0f9a3985681d2a7f518a69b9c3b1f8d4aae0
mangeshnag9651 <98649695+mangeshnag9651@users.noreply.github.com>
2024-05-22 17:22:09 +0530

    Create vars.tf

---
+++ terraform/vars.tf
@@ -0,0 +1,19 @@
+variable "prod_lb_dns_name" {
+  description = "DNS name of the prod load balancer"
+  default     = "abgfbfgbfgbfggbfgbgfbgfbggbggbg"
+}
+
+variable "prod_lb_zone_id" {
+  description = "Zone ID of the prod load balancer"
+  default     = "Zxdafreeveeeeeeeeeeeeeeeefe"
+}
+
+variable "test_lb_dns_name" {
+  description = "DNS name of the test load balancer"
+  default     = "ad42frgytbytbybynrynryunyurnunyujujyu"
+}
+
+variable "test_lb_zone_id" {
+  description = "Zone ID of the test load balancer"
+  default     = "Zrvvgrgtrvgtvrtvrgtvgtvrtvrtvtvtvvh"
+}
